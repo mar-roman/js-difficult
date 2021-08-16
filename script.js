1b3a78ab7bee1375a0fa8a1ab8041182ba6b8e3d
mar-roman <roman.markaryants@yandex.ru>
2021-08-16 10:20:53 +0300

    perform lesson13

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const body = document.querySelector('body'),
+      wrapper = document.querySelector('.wrapper'),
+      color = document.getElementById('color'),
+      change = document.getElementById('change');
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+change.addEventListener('click', function(){
+  let newColor = getRandomColor();
+  body.style.backgroundColor = newColor;
+  change.style.color = newColor;
+  color.textContent = newColor;
+});
