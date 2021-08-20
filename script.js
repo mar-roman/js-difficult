be8d0c058fb9b85fa82221c3be4092cb83ca35d8
mar-roman <roman.markaryants@yandex.ru>
2021-08-20 11:01:20 +0300

    fix mistakes

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ function DomElement(){
   this.height = ''; 
   this.width = ''; 
   this.bg = ''; 
-  this.fontSize = '';
-  this.color = '';
   this.position = 'absolute';
 }
 
-DomElement.prototype.create = function(){
-  this.selector = prompt('Введите селектор');
+DomElement.prototype.create = function(selector){
+  this.selector = selector;
   if(this.selector[0] === '.'){
     document.querySelector('body').innerHTML += '<div class="' + this.selector.slice(1) + '">' + '</div>';
     document.querySelector(this.selector).style.cssText += 'position: ' + this.position + ';';
@@ -29,42 +27,24 @@ DomElement.prototype.addText = function(){
   document.querySelector(tag).innerHTML += 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
 };
 
-DomElement.prototype.addColor = function(){
-  let tag = prompt('Какой тег надо изменить?');
-  let color = prompt('Напишите цвет');
-  document.querySelector(tag).cssText += 'color: ' + color + ';';
+DomElement.prototype.changeHeight = function(value){
+  document.querySelector(this.selector).style.cssText += 'height: ' + value + ';';
 };
 
-DomElement.prototype.changeHeight = function(){
-  let tag = prompt('Какой тег надо изменить?');
-  this.height = prompt('Укажите значение высоты');
-  document.querySelector(tag).style.cssText += 'height: ' + this.height + ';';
+DomElement.prototype.changeWidth = function(value){
+  document.querySelector(this.selector).style.cssText += 'width: ' + value + ';';
 };
 
-DomElement.prototype.changeWidth = function(){
-  let tag = prompt('Какой тег надо изменить?');
-  this.width = prompt('Укажите значение ширины');
-  document.querySelector(tag).style.cssText += 'width: ' + this.width + ';';
-};
-
-DomElement.prototype.changeBg = function(){
-  let tag = prompt('Какой тег надо изменить?');
-  this.bg = prompt('Укажите значение фона');
-  document.querySelector(tag).style.cssText += 'background: ' + this.bg + ';';
-};
-
-DomElement.prototype.changeFontSize = function(){
-  let tag = prompt('Какой тег надо изменить?');
-  this.changeFontSize = prompt('Укажите значение размера текста');
-  document.querySelector(tag).style.cssText += 'font-size: ' + this.changeFontSize + ';';
+DomElement.prototype.changeBg = function(value){
+  document.querySelector(this.selector).style.cssText += 'background: ' + value + ';';
 };
 
 document.addEventListener("DOMContentLoaded", function(){
   let element = new DomElement();
-  element.create(); //Создать див
-  element.changeHeight(); //Ввести 100px
-  element.changeWidth(); //Ввести 100px
-  element.changeBg(); //Ввести red
+  element.create('.elem');
+  element.changeHeight('100px');
+  element.changeWidth('100px');
+  element.changeBg('red');
 
   let left = 0;
   let top = 0;
@@ -77,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function(){
         break;
       case 38:
         top -= 10;
-        document.querySelector(element.selector).style.cssText += 'top: ' + left + 'px;';
+        document.querySelector(element.selector).style.cssText += 'top: ' + top + 'px;';
         break;
       case 39:
         left += 10;
@@ -85,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function(){
         break;
       case 40:
         top += 10;
-        document.querySelector(element.selector).style.cssText += 'top: ' + left + 'px;';
+        document.querySelector(element.selector).style.cssText += 'top: ' + top + 'px;';
         break;
     }
   };
